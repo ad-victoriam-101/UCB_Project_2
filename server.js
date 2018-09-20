8c24214438554f601732bd5be886718f3f7d3ef7
Isaac Romarri <iromero385@berkeley.edu>
2018-09-19 21:20:14 -0700

    deploying

--- server.js
+++ server.js
@@ -35,4 +35,5 @@ db.sequelize.sync({}).then(function(){
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
+  
 });
